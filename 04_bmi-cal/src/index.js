1c9dcbef6a864b7b194de8b4d3d3e3746abb4d1b
Abhay12kumar <abhayverma20035@gmail.com>
2025-01-18 13:47:35 +0530

    Abhay

---
+++ 04_bmi-cal/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+import Calculator from './01-Calculator';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Calculator/>
+  </React.StrictMode>
+);
